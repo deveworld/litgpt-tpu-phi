c04a06f65e7bdea84eac271b49a342aea7cc1e7f
World <world@worldsw.dev>
2024-10-25 21:24:40 +0900

    litgpt-tpu-phi

--- train.sh
+++ train.sh
@@ -1,8 +1,6 @@
 export USER=$(whoami)
 export TPU_NAME='v4-64'
 export ZONE='us-central2-b'
-export BUCKET=''
-export DATASET=''
 
 echo "[local] Killing TPU"
 gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME \
@@ -29,12 +27,12 @@ $$ cd ~/litgpt-tpu-phi/ && git pull"
 
 echo "[local] Install_dependencies"
 gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME --zone $ZONE --worker=all --command \
-"cd ~/litgpt-phi-tpu; bash install_dependencies.sh; "
+"cd ~/litgpt-tpu-phi; bash install_dependencies.sh; "
 
 echo "[local] Set runner.sh"
-gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME --zone $ZONE --worker=all --command "chmod +x /home/${USER}/litgpt-phi-tpu/py/runner.sh"
+gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME --zone $ZONE --worker=all --command "chmod +x /home/${USER}/litgpt-tpu-phi/py/runner.sh"
 
 echo "[local] RUN!!!"
-gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME --zone us-central2-b --worker=all --command \
+gcloud compute tpus tpu-vm ssh $USER@$TPU_NAME --zone $ZONE --worker=all --command \
 "screen -L -d -m bash -i -c 'export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=107374182400; \
-cd litgpt-phi-tpu; /home/${USER}/litgpt-phi-tpu/py/runner.sh'"
+cd litgpt-tpu-phi; /home/${USER}/litgpt-tpu-phi/py/runner.sh'"
